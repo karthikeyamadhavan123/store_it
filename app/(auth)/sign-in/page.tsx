0e187d9108195e40d0634fec2d1763d58e0d0b9c
karthikeyamadhavan123 <karthikeyadonnipad2005@gmail.com>
2025-02-07 14:17:10 +0530

    second commit

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SignIn = () => {
+  return (
+    <div>
+    <h1>Hello, I am Sign In</h1>
+    </div>
+  )
+}
+
+export default SignIn
---
+++ components/AuthForm.tsx
@@ -0,0 +1,12 @@
+ import React from 'react'
+ 
+ const AuthForm = () => {
+   return (
+     <div>
+       AuthForm
+     </div>
+   )
+ }
+ 
+ export default AuthForm
+ 
