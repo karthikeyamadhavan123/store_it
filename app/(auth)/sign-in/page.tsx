2fb4b9876c9aad1a7e0fd82d3cd7f1dbadd6dd60
karthikeyamadhavan123 <karthikeyadonnipad2005@gmail.com>
2025-02-07 14:57:28 +0530

    third commit

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,11 +1,6 @@
+import AuthForm from '@/components/AuthForm'
 import React from 'react'
 
-const SignIn = () => {
-  return (
-    <div>
-    <h1>Hello, I am Sign In</h1>
-    </div>
-  )
-}
+const SignIn = () => <AuthForm type="sign-in"/>
 
 export default SignIn
